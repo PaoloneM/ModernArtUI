ae121a18de9658f49b7c26576be08f881e82fce5
Paolo Morgano <paolo.morgano@gmail.com>
2014-10-28 07:56:16 +0100

    First layout draft

--- gen/com/example/modernartui/R.java
+++ gen/com/example/modernartui/R.java
@@ -25,7 +25,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int Tw1=0x7f080000;
+        public static final int Tw2=0x7f080001;
+        public static final int Tw3=0x7f080002;
+        public static final int Tw4=0x7f080003;
+        public static final int Tw5=0x7f080004;
+        public static final int action_settings=0x7f080006;
+        public static final int seekBar1=0x7f080005;
     }
     public static final class layout {
         public static final int activity_modern_art=0x7f030000;
